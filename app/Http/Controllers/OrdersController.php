57006395b82cbbb4733c1b2dbd6090b3f0bb12b0
Jaroslav Lomecki <jaroslavlo@gmail.com>
2018-03-30 13:45:35 +0300

    Create orders filter

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\ChangeOrderStatusRequest;
 use App\Invoice;
 use App\Order;
 use App\OrderProduct;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
@@ -26,18 +27,41 @@ class OrdersController extends Controller
         $this->checkInvoice = $invoiceService;
     }
 
-    public function index()
+    public function index(Request $request)
     {
         $user=Auth::user();
         if($user->role == 'admin')
         {
             $orders = Order::paginate(20);
+	        $user = User::all();
+            if ($request->has('filter')){
+            	$orders = new Order;
+            	if($request->get('user_id') >0){
+	            $orders = $orders->where('user_id', $request->get('user_id'));
+	            }
+	            if($request->get('type') >= 0){
+		            $orders = $orders->where('type', $request->get('type'));
+	            }
+	            if($request->get('status') >= 0){
+		            $orders = $orders->where('status', $request->get('status'));
+	            }
+	            $orders = $orders->paginate(20);
+            }
         }else{
             $orders =$user->orders()->paginate(20);
+	        if ($request->has('filter')){
+	        	$orders = new Order();
+		        if($request->get('type') >= 0){
+			        $orders = $orders->where('user_id', $user->id)->where('type', $request->get('type'));
+		        }
+		        if($request->get('status') >= 0){
+			        $orders = $orders->where('user_id', $user->id)->where('status', $request->get('status'));
+		        }
+		        $orders = $orders->paginate(20);
+	        }
         }
-
         return view('orders.orders', [
-            'orders'=>$orders,
+            'orders'=>$orders, 'users'=>$user
         ]);
     }
     public function show($id)
