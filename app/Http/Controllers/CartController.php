bfbe04fc7e455f3b2b359ec8b4565343b4782a5b
aivars17 <amalosevskis@gmail.com>
2018-04-03 13:46:35 +0300

    fixed bug with orders chat, change delete function

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -187,7 +187,6 @@ class CartController extends Controller
         }
         $userEmail = Auth::user()->client->email;
         Mail::to($userEmail)->send(new OrderReceived($order, $backOrder, $preOrder, $orderComment));
-
         return redirect()->back();
     }
 }
