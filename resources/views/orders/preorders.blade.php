86938331f1be486f65ca8b9a5a2ce9bfd1592fa1
aivars17 <amalosevskis@gmail.com>
2018-04-04 13:55:10 +0300

    show unconfirmed preorders before deadline

---
+++ resources/views/orders/preorders.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Edis
+ * Date: 4/4/2018
+ * Time: 12:57 PM
+ */
---
+++ resources/views/orders/single_preorder.blade.php
@@ -0,0 +1,104 @@
+<?php
+@extends('layouts.page')
+@section('content')
+    @inject('cartService', "App\Services\CartService")
+    <div class="col-10 mt-5">
+        <!-- Order table -->
+        @if(!empty($preorders))
+            <div class="row">
+                <div class="col-md-12 table-responsive">
+                    <table class="table table-sm">
+                        <h3>PRE-ORDER</h3>
+                        <thead class="thead-light">
+                        <tr>
+                            <th scope="col">EAN:</th>
+                            <th scope="col">Platform:</th>
+                            <th scope="col">Name:</th>
+                            <th scope="col">Release date:</th>
+                            <th scope="col">Publisher:</th>
+                            <th scope="col">Price:</th>
+                            <th scope="col">Price Total:</th>
+                            <th scope="col">Quantity</th>
+                            <th scope="col"></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($preorders as $P_product)
+                            <tr>
+                                <td data-label="EAN:" class="align-middle text-right text-lg-center">{{ $P_product->product->ean }}</td>
+                                <td data-label="Platform:" class="align-middle text-right text-lg-center">{{ $P_product->product->platform->name }}</td>
+                                <td data-label="Name:" class="align-middle text-right text-lg-center">{{ $P_product->product->name }}</td>
+                                <td data-label="Release date:" class="align-middle text-right text-lg-center">{{ $P_product->product->release_date }}</td>
+                                <td data-label="Publisher:" class="align-middle text-right text-lg-center">{{ !empty($P_product->product->publisher) ? $P_product->product->publisher->name : '' }}</td>
+                                <td data-label="Price:" class="align-middle text-right text-lg-center">{{ number_format($P_product->product->PriceAmount, 2, '.', '') }} €</td>
+                                <td id="singlePrice_P{{ $P_product->id }}" data-label="Price:" class="align-middle text-right text-lg-center">{{ number_format($cartService->getSingleProductPrice($P_product), 2, '.', '') }} €</td>
+                                <td data-label="Quantity:" class="align-middle text-right text-lg-center">
+                                    <input data-index="P" min="1" data-url="{{ route('order.update',$P_product->id) }}" class="input setquantity_BP text-right" type="number" name="amount" value="{{ $P_product->quantity }}">
+                                    <br>
+                                    <span id="message{{ $P_product->id }}" ></span>
+                                </td>
+                                <td class="align-middle text-right text-lg-center">
+                                    <button class="btn btn-danger btn-sm delete" data-html="{{ route('order.index') }}" data-url="{{ route('order.product.delete', $P_product->id) }}">Delete</button>
+                                </td>
+                            </tr>
+                        @endforeach
+                        <tr>
+                            <td scope="total" colspan="6" class="text-right"><b>Total</b></td>
+                            <td class="align-middle text-right text-lg-center" id="totalPrice_P" rowspan="6" data-label="Total">{{ !empty($preorders) ? number_format($cartService->getTotalCartPrice($preorders->first()->order), 2, '.', '') : ''}} €</td>
+                            <td class="align-middle text-right text-lg-center" id="totalQuantity_P" data-label="Total quantity">{{ !empty($preorders) ? $cartService->getTotalCartQuantity($preorders->first()->order) : '' }}</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        @endif
+        <div>
+            @if(!empty($product))
+                <input type="hidden" name="order_id" value="{{$order->id}}">
+            @endif
+            @if(!empty($backorder))
+                <input type="hidden" name="backorder_id" value="{{$backorder->id}}">
+            @endif
+            @if(!empty($preorder))
+                <input type="hidden" name="preorder_id" value="{{$preorder->id}}">
+            @endif
+            @if(!empty($product) || !empty($backorder) || !empty($preorder))
+                <div class="row mb-2">
+                    <div class="col-12">
+                        <a class="btn btn-dark btn-block" href="{{ route('home') }}">Back to Shop</a>
+                    </div>
+                </div>
+            @else
+                <h3 class="text-center">Your cart is empty</h3>
+                <a class="btn btn-dark btn-block" href="{{ route('home') }}">Back to Shop</a>
+            @endif
+        </div>
+        <!-- Comments and attachments -->
+        @if(!empty($order) || !empty($backorder) || !empty($preorder))
+            <div class="row">
+                <div class="col-12 mt-2">
+                    <form action="{{ route('cart.confirm') }}" method="post">
+                        @csrf
+                        <div class="form-group">
+                            <label for="exampleFormControlTextarea1"><h3>Comments</h3></label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" name="comments" rows="6"></textarea>
+                        </div>
+                        @if(!empty($product))
+                            <input type="hidden" name="order_id" value="{{$order->id}}">
+                        @endif
+                        @if(!empty($backorder))
+                            <input type="hidden" name="backorder_id" value="{{$backorder->id}}">
+                        @endif
+                        @if(!empty($preorder))
+                            <input type="hidden" name="preorder_id" value="{{$preorder->id}}">
+                        @endif
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-danger btn-block" >Confirm your order</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        @endif
+    </div>
+    </div>
+@endsection
